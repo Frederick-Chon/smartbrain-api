5a92ea7169baf4d7f90d5d6cd24ba81bc00803b3
Frederick Chon <frederick.chon@gmail.com>
2019-04-11 15:25:48 -0700

    setup CORS middleware to link with front-end

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -32,7 +35,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
-            res.json('success!');
+            res.json('success');
     } else {
         res.status(400).json('Error logging in!');
     }
