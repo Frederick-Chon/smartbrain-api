1d68bc623a5682e065bbf880f9bf0e655571534f
Frederick Chon <frederick.chon@gmail.com>
2019-04-15 14:42:43 -0700

    completed /signin endpoint to check with our postgreSQL database when users login

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-})
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -49,12 +45,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]); 
-    } else {
-        res.status(400).json('Error logging in!');
-    }
+    db.select('email', 'hash').from('login') 
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            } 
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 });
 
 app.post('/register', (req, res) => {
